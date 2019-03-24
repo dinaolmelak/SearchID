ba3450bca894b0d37d26eead3c068a22c4dd3870
Dinaol Tadesse <dinaolmelak@gmail.com>
2019-03-24 11:09:38 -0500

    App for finding people ID

---
+++ SearchID/SecondViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SecondViewController.swift
+//  SearchID
+//
+//  Created by Dinaol Melak on 3/24/19.
+//  Copyright © 2019 Dinaol Melak. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SecondViewController: UIViewController {
+    
+    //@IBOutlet weak var displayField: UILabel!
+    // var inputText: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //performSegue(withIdentifier: "display", sender: self)
+        // Do any additional setup after loading the view.
+    }
+    /*
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let dinaol = segue.destination as! ViewController
+        dinaol.textField.text=inputText.text
+        
+    }*/
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SearchID/ViewController.swift
+++ SearchID/ViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    //@IBOutlet weak var textField: UITextField!
+    //@IBOutlet weak var showLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //performSegue(withIdentifier: "search", sender: self)
+        
     }
-
+    /*
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let dinaol = segue.destination as! SecondViewController
+        showLabel.text=dinaol.displayField.text
+        
+    }*/
+    
+    
+    
+    
 
 }
 
