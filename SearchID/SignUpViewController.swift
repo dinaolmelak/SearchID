2c0e97628f9943986cfb7551db679a938d8552b4
Dinaol Tadesse <dinaolmelak@gmail.com>
2019-04-03 21:51:21 -0500

    Got to switch from main to sign up View

--- SearchID/SignUpViewController.swift
+++ SearchID/SignUpViewController.swift
@@ -10,23 +10,12 @@ import UIKit
 import Firebase
 
 class SignUpViewController: UIViewController {
-    
-    @IBOutlet weak var outputLabel: UILabel!
-    var outputLabelText: String!
     //@IBOutlet weak var displayField: UILabel!
     // var inputText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        outputLabel.text = outputLabelText
         //performSegue(withIdentifier: "display", sender: self)
         // Do any additional setup after loading the view.
-    }
-    @IBAction func didTapOnCancel(_ sender: Any) {
-        let firstVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "firstVC") as! ViewController
-        self.addChild(firstVC)
-        self.view.addSubview(firstVC.view)
-        firstVC.didMove(toParent: self)
-        
     }
     
     
